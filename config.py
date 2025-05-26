49a5a6130e5fe0618a992f416441071aaa0ac0ca
Naman794 <namansinha794@gmail.com>
2025-05-26 14:54:55 +0530

    changes

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
     GUILD_ID = int(os.getenv("GUILD_ID", "720914167652941846"))
     DISCORD_CLIENT_ID = os.getenv("CLIENT_ID", "741322048726368286")
     DISCORD_CLIENT_SECRET = os.getenv("CLIENT_SECRET", "t90BwTah09VJqpTPU7e9X-yxjrGZ-KW9")
-    DISCORD_REDIRECT_URI = os.getenv("REDIRECT_URI", "http://dono-01.danbot.host:1646/callback")
+    DISCORD_REDIRECT_URI = os.getenv("REDIRECT_URI", "https://webapp-testing-ys3o.onrender.com/callback")
     
     OAUTH2_SCOPE = ["identify", "guilds.join"]
     OAUTH2_BASE_URL = "https://discord.com/oauth2/authorize"
