b3c16f45420577660f2c990d4e8f0b66860f0542
Naman794 <namansinha794@gmail.com>
2025-05-26 14:38:53 +0530

    port update

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def redirect_unauthorized(e):
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 1646))
