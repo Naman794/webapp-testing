068480c1057a13f6d01cd2167acbd960b223511f
Naman794 <namansinha794@gmail.com>
2025-05-28 14:43:08 +0530

    config update

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "669218CABDA3BDE5")
 GUILD_ID = int(os.getenv("GUILD_ID", "720914167652941846"))
 DISCORD_CLIENT_ID = os.getenv("CLIENT_ID", "741322048726368286")
 DISCORD_CLIENT_SECRET = os.getenv("CLIENT_SECRET", "t90BwTah09VJqpTPU7e9X-yxjrGZ-KW9")
-DISCORD_REDIRECT_URI = os.getenv("REDIRECT_URI", "https://127.0.0.1:5000/callback")
+DISCORD_REDIRECT_URI = os.getenv("REDIRECT_URI", "https://webapp-testing-1eo5.onrender.com/callback")
 
 OAUTH2_SCOPE = ["identify", "guilds.join"]
 OAUTH2_BASE_URL = "https://discord.com/oauth2/authorize"
